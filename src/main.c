ba3b4cfaf9bfb5d2858ecfd32dc4b5c2c3e16a58
Renata Azevedo <razevedo@c2r2s6.42porto.com>
2026-07-29 16:25:45 +0100

    extract number of coders

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: razevedo <razevedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 15:21:35 by razevedo          #+#    #+#             */
-/*   Updated: 2026/07/29 15:36:24 by razevedo         ###   ########.fr       */
+/*   Updated: 2026/07/29 16:23:44 by razevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,17 @@ void	*compute()
 	return result;
 }
 
+int	get_nb_of_coders(char **args)
+{
+	return (atoi(args[0]));
+}
+
 int	main(int argc, char **argv)
 {
 	char		**args;
 	pthread_t	thread0;
 	void		*return_value;
+	int			number_of_coders;
 
 	args = get_args(argc, argv);
 	if (!args)
@@ -33,6 +39,9 @@ int	main(int argc, char **argv)
 		write(2, "Error: invalid arguments.\n", 26);
 		return (0);
 	}
+	
+	number_of_coders = get_nb_of_coders(args);
+	printf("Number of coders: %d\n", number_of_coders);
 	
 	printf("ARGS:\n");
 	for (int i = 0; i < 8; i++)
